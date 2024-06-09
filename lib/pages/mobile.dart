1eae0f6d4afb677d62b68ab184b6ab772a8a6ed5
sanin <saninthottungalhere@gmail.con>
2024-06-09 13:08:49 +0530

    items in all screens made available

--- lib/pages/mobile.dart
+++ lib/pages/mobile.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:product_manager/pages/add_item.dart';
+import 'package:product_manager/providers/item_provider.dart';
 
-class ScreenMobile extends StatelessWidget {
+class ScreenMobile extends ConsumerWidget {
   const ScreenMobile({super.key, this.title});
   final String? title;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final items = ref.watch(itemsProvider);
+    final mobiles = items.where((model) => model.itemType == ShopItems.mobile);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Mobiles'),
       ),
-      body: const Center(
-        child: Text("mobiles"),
-      ),
+      body: mobiles.isEmpty
+          ? const Center(
+              child: Text("No items in stock"),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text(
+                      index.toString(),
+                    ),
+                  ),
+                  title: Text(mobiles.elementAt(index).title),
+                );
+              },
+              itemCount: mobiles.length,
+            ),
     );
   }
 }
--- lib/pages/speaker.dart
+++ lib/pages/speaker.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:product_manager/pages/add_item.dart';
+import 'package:product_manager/providers/item_provider.dart';
 
-class ScreenSpeaker extends StatelessWidget {
+class ScreenSpeaker extends ConsumerWidget {
   const ScreenSpeaker({super.key, this.title});
   final String? title;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final items = ref.watch(itemsProvider);
+    final speakers =
+        items.where((element) => element.itemType == ShopItems.speaker);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Speakers'),
       ),
-      body: const Center(
-        child: Text("speaker"),
-      ),
+      body: speakers.isEmpty
+          ? const Center(
+              child: Text("No watches in stock"),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text(index.toString()),
+                  ),
+                  title: Text(speakers.elementAt(index).title),
+                );
+              },
+              itemCount: speakers.length,
+            ),
     );
   }
 }
